866254ebaa850ca5b2ca95258dcb445b520c0062
Yigit Tuncer <yigit_tuncer03@hotmail.com>
2026-07-24 14:54:22 +0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,27 @@
+import string
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        chars = list(string.ascii_letters)
+        chars.extend(map(str, range(10)))
+        print(chars)
+
+        while i < j:
+            if s[i] not in chars:
+                i += 1
+                continue 
+            elif s[j] not in chars:
+                j -= 1
+                continue
+            elif s[i].lower() != s[j].lower():
+                return False
+            else:
+                i += 1
+                j -= 1
+        return True
+
+
+        
