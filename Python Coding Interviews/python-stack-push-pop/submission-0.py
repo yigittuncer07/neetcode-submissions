1467000f32c564960e6a300a1e008747ae2b6cb2
Yigit Tuncer <yigittuncer22@gmail.com>
2026-09-02 22:56:07 +0300

    Add: python-stack-push-pop - submission-0

---
+++ Python Coding Interviews/python-stack-push-pop/submission-0.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    return arr[::-1]
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
