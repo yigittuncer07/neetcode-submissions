820125ea3e4bfbfcce85355ac061a645e6eca56a
Yigit Tuncer <yigittuncer22@gmail.com>
2026-09-05 19:12:50 +0300

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.py
@@ -0,0 +1,24 @@
+class Twitter:
+
+    def __init__(self):
+        self.posts = collections.defaultdict(list)  # userId -> list of (timestamp, tweetId)
+        self.follow_map = collections.defaultdict(set)
+        self.time = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.posts[userId].append((self.time, tweetId))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        candidates = self.posts[userId][-10:]
+        for u_id in self.follow_map[userId]:
+            candidates = candidates + self.posts[u_id][-10:]
+
+        candidates.sort(key=lambda x: x[0], reverse=True)
+        return [tweetId for _, tweetId in candidates[:10]]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follow_map[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.follow_map[followerId].discard(followeeId)
