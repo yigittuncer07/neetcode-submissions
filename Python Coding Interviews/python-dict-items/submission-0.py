21b76b4d20125c2416b3f7493721cd6fb618ff66
Yigit Tuncer <yigittuncer22@gmail.com>
2026-09-06 01:39:08 +0300

    Add: python-dict-items - submission-0

---
+++ Python Coding Interviews/python-dict-items/submission-0.py
@@ -0,0 +1,12 @@
+from typing import Dict, List, Tuple
+
+
+def get_dict_items(age_dict: Dict[str, int]) -> List[Tuple[str, int]]:
+    return [(name, age) for name, age in age_dict.items()]
+
+
+# do not modify below this line
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}))
+print(get_dict_items({'Bob': 30, 'David': 40, 'Charlie': 35, 'Alice': 25, 'Eve': 45}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45, 'Frank': 50}))
