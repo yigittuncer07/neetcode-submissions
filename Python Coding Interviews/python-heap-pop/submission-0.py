80494543c4f94f22fcc4c9a7fbf3821567d05a04
Yigit Tuncer <yigittuncer22@gmail.com>
2026-09-06 13:53:30 +0300

    Add: python-heap-pop - submission-0

---
+++ Python Coding Interviews/python-heap-pop/submission-0.py
@@ -0,0 +1,12 @@
+import heapq
+from typing import List
+
+
+def heap_pop(heap: List[int]) -> List[int]:
+    return [heapq.heappop(heap) for _ in range(len(heap))]
+
+
+# do not modify below this line
+print(heap_pop([1, 2, 3]))
+print(heap_pop([1, 3, 2]))
+print(heap_pop([6, 7, 8, 12, 9, 10]))
