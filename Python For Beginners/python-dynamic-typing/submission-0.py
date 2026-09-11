4a86fc9c4f314a819f79695ca8e8369e9fffe17f
Yigit Tuncer <yigittuncer22@gmail.com>
2026-09-11 17:42:35 +0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 0
+print(type(variable))
+variable = 0.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = ""
+print(type(variable))
+variable = []
+print(type(variable))
